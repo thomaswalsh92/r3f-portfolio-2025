11e66d50e3e5f67705ccfa65156e870c0352447b
Thomas Walsh <thomaseawalsh@gmail.com>
2025-02-14 17:55:19 +0000

    Carousel pre-refactor

--- src/app/components/Carousel.jsx
+++ src/app/components/Carousel.jsx
@@ -49,6 +49,8 @@ const Carousel = () => {
 
   const [focused, setFocused] = useState(0);
 
+  const [carouselOffset, setCarouselOffset] = useState(0);
+
   const ref = useRef(null);
   useEffect(() => {
     setHeight(ref.current.clientHeight);
@@ -67,7 +69,13 @@ const Carousel = () => {
   const margin = 128;
   const range = width - (margin * 2); /*prettier-ignore*/
   const start = margin;
-  const numVisible = Math.floor(range / (collapsedWidth + gap)); /*prettier-ignore*/
+  let numVisible = Math.floor(range / (collapsedWidth + gap)); /*prettier-ignore*/
+
+  //force odd number of items
+  if (numVisible % 2 === 1) {
+    numVisible = numVisible + 1;
+  }
+
   const spaceForVisible = (collapsedWidth + gap) * numVisible;
   const remainder = range - spaceForVisible;
 
@@ -76,26 +84,55 @@ const Carousel = () => {
   };
 
   const calculateLeft = (i) => {
-    const offset = (start + ((collapsedWidth + gap) * i)) - (collapsedWidth / 2) + (remainder / 2) /*prettier-ignore*/
-    return offset;
+    return (start + ((collapsedWidth + gap) * i)) - (collapsedWidth / 2) + (remainder / 2) /*prettier-ignore*/
   };
 
-  //what we need now is a function that computes
+  const debugGetColor = (i) => {
+    const select = i % 3;
+    switch (select) {
+      case 0:
+        return "red";
+      case 1:
+        return "blue";
+      case 2:
+        return "green";
+      default:
+        "white";
+    }
+  };
+
+  const handleLeft = () => {
+    setCarouselOffset(carouselOffset - 1);
+  };
+
+  const handleRight = () => {
+    setCarouselOffset(carouselOffset + 1);
+  };
+
+  const focusedItem = numVisible / 2;
 
   return (
     <div className="carouselContainer" ref={ref}>
+      <button style={{ marginTop: 256 }} onClick={() => handleLeft()}>
+        LEFT
+      </button>
+      <button
+        style={{ marginTop: 256, marginLeft: 16 }}
+        onClick={() => handleRight()}
+      >
+        RIGHT
+      </button>
       {[...projects, ...projects, ...projects].map((project, i) => {
-        console.log(i);
         if (i <= numVisible) {
           return (
             <CarouselItem
               style={{
                 position: "absolute",
                 height: collapsedHeight,
-                width: collapsedWidth,
+                width: focusedItem === i ? collapsedWidth * 2 : collapsedWidth,
                 top: calculateTop(i),
                 left: calculateLeft(i),
-                backgroundColor: "red",
+                backgroundColor: debugGetColor(i),
               }}
               project={project}
               index={i}
